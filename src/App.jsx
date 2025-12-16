976d144e54753c2f9308f20d54fac9379cc128f3
ayeshalatif1 <ayeshalatifchoudhery@gmail.com>
2025-12-16 22:36:27 +0500

    all

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       </Route>
     ),
     {
-      basename:"/interactiveRatingComponent"
+      basename:"/https://ayeshalatif1.github.io/interactiveRatingComponent/"
     }
 
   );
